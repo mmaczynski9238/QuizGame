20e883c1f7da730484bcda1b43294ebbde43a21f
student3 <student3@jh-mba158-28.d214.org>
2016-10-19 09:43:50 -0500

    First Commit

--- ReviewS/ViewController.swift
+++ ReviewS/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ReviewS
 //
-//  Created by student3 on 10/19/16.
+//  Created by JBM on 10/19/16.
 //  Copyright © 2016 John Hersey High School. All rights reserved.
 //
 
@@ -12,12 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
