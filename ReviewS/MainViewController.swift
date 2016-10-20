fd9b80b753fc3f2302cd777bc9abccb9185fb640
student3 <student3@jh-mba158-28.d214.org>
2016-10-20 10:41:59 -0500

    Added login page

--- ReviewS/MainViewController.swift
+++ ReviewS/MainViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+    }
 
 }
 
