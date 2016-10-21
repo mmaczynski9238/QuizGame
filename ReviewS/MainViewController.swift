c515288f862a0a8596bb4b606a050ecb6601f5b6
student5 <student3@jh-mba158-27.d214.org>
2016-10-21 10:00:44 -0500

    added login .5 done

--- ReviewS/MainViewController.swift
+++ ReviewS/MainViewController.swift
@@ -16,10 +16,36 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
     
     @IBAction func loginButton(_ sender: UIButton) {
+        let user = usernameField.text
+        let pass = passwordField.text
+        
+        let usernameStored = UserDefaults.standard.string(forKey: "username")
+        let passwordStored = UserDefaults.standard.string(forKey: "password")
+        
+        if usernameStored == user{
+            if passwordStored == pass{
+                UserDefaults.standard.set(true,forKey: "isLoggedIn")
+                UserDefaults.standard.synchronize()
+                self.dismiss(animated: true, completion: nil)
+                let viewController1: ViewController1 = ViewController1()
+                viewController1.view.backgroundColor = UIColor.blue
+                
+                self.present(viewController1, animated: true, completion: nil)
+            }
+            
+            else if passwordStored != pass{
+                let viewController1: ViewController1 = ViewController1()
+                viewController1.view.backgroundColor = UIColor.red
+                }
+                
+            
+            
+        }
         
     }
 
