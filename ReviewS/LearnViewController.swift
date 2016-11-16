0b2ce89e9295ccb837f0185c853e6a099eae16e9
student5 <student3@jh-mba158-27.d214.org>
2016-11-16 09:58:52 -0600

    Added outlets

---
+++ ReviewS/LearnViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LearnViewController.swift
+//  ReviewS
+//
+//  Created by student3 on 11/16/16.
+//  Copyright © 2016 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    
+    @IBOutlet weak var termLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var correctLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    
+    
+    
+
+}
