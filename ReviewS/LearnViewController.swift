37dc3d5af1b225439f9289d7aba4c4e668175909
student5 <student3@jh-mba158-27.local>
2016-11-30 09:33:38 -0600

    updated learn controller

--- ReviewS/LearnViewController.swift
+++ ReviewS/LearnViewController.swift
@@ -14,15 +14,48 @@ class LearnViewController: UIViewController {
     @IBOutlet weak var termLabel: UILabel!
     @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var correctLabel: UILabel!
+    @IBOutlet weak var incorrectLabel: UILabel!
+    
+    
+    
+    typealias termAnswers = (String, String)
+    var termAnswer: [termAnswers] = [termAnswers]()
+    var score = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        termAnswer.append(("brent","gurvis"))
+        termAnswer.append(("Mac and", "cheese"))
+        print(termAnswer.last?.0)
+        
+        
+        termLabel.text = termAnswer.last?.0
+        
+        
+    }
+    
+    @IBAction func checkButton(_ sender: AnyObject) {
+        if answerTextField.text == termAnswer.last?.1
+        {
+            correctLabel.text = "\(score)"
+            score += 1
+        }
+        else{
+            incorrectLabel.text = "\(score)"
+            score += 1
+            
+        }
 
     }
-
     
-    
-    
-
 }
+    
+    
+
+    
+    
+
+
