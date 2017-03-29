784076682c3093e6d395ee92060bf6fafeb24283
Michael Maczynski <mikemaczynski@Michaels-MacBook-Pro.local>
2017-03-29 00:07:44 -0500

    changed layout of test screen

--- ReviewS/TestViewController.swift
+++ ReviewS/TestViewController.swift
@@ -19,6 +19,7 @@ struct Question{
 
 class TestViewController: UIViewController {
     
+    @IBOutlet var currentQuestionLabel: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -30,7 +31,16 @@ class TestViewController: UIViewController {
     @IBOutlet weak var theEnd: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var restartBtn: UIButton!
+    @IBOutlet var labelButton1: UILabel!
+    @IBOutlet var labelButton2: UILabel!
+    @IBOutlet var labelButton3: UILabel!
+    @IBOutlet var labelButton4: UILabel!
     var counter = 1
+    
+    var CQCount = 0
+    
+    var numOfQuestions = 0
+
     
     var timerCounter = 0
     
@@ -76,35 +86,52 @@ class TestViewController: UIViewController {
         scoreLbl = UILabel(frame: CGRect(x: 35, y: 45, width: 77, height: 45))
         scoreLbl.textAlignment = NSTextAlignment.center
         scoreLbl.text = "0"
+        scoreLbl.textColor = UIColor.white
         self.scoreLbl.font = UIFont(name: "HelveticaNeue", size:35)
         self.view.addSubview(scoreLbl)
         
         pickQuestions()
+        findCount()
+    }
+    
+    func findCount()
+    {
+        numOfQuestions = Questions.count
+    }
+    
+    func findCurrentQuestion()
+    {
+        
+        
+        CQCount += 1
+        
+        if CQCount == numOfQuestions+1 {
+            var count = 0
+        }
+        currentQuestionLabel.text = "\(CQCount)" + "/" + "\(numOfQuestions)"
         
     }
     
-    
-    
     func resetQuestions(){
         Questions = [Question(Question: "What is the Biggest Hit of Bing Crosby?" , Answers: ["Swinging on a Star", "Now is the Hour", "White Christmas", "Beautiful Dreamer"], Answer: 2),
-                     Question(Question: "What is Elvis Presely's Middle Name?", Answers: ["Aaron", "Micheal", "George", "Matthew"], Answer: 0),
+                     Question(Question: "What is Elvis Presely's Middle Name?", Answers: ["Aaron", "Michael", "George", "Matthew"], Answer: 0),
                      Question(Question: "How Many Oscars did Titanic win?", Answers: ["5", "7", "10", "11"], Answer: 3),
                      Question(Question: "From which country did Pita Bread originate?", Answers: ["Spain", "France", "Greece", "Russia"], Answer: 2),
                      Question(Question: "What is the largest living creature on Earth?", Answers: ["Whale", "Shark", "Sea Turtle", "Alligator"], Answer: 0),
-                     Question(Question: "What does ATM stand for?", Answers: ["Automatic Treasure Machine", "Automatic Tax Machine", "Anti Tax Machine", "Automatic Teller Machine"], Answer: 3),
+                     Question(Question: "What does ATM stand for?", Answers: ["Automatic Treasure Machine", "Automatic Tax Machine", "Anti-Tax Machine", "Automatic Teller Machine"], Answer: 3),
                      Question(Question: "What's the world's second largest French speaking city?", Answers: ["Paris", "Montreal", "Versailles", "Québec"], Answer: 1),
                      Question(Question: "Which country is the largest exporter of potassium?", Answers: ["Kazakhstan", "France", "Greece", "Spain"], Answer: 0),
                      Question(Question: "How long is the Great Wall of China?", Answers: ["3200 miles", "4000 miles", "2000 kilometers", "4500 miles"], Answer: 1),
                      Question(Question: "Who is on the 10 dollar bill?", Answers: ["George Washington", "Thomas Jefferson", "Alexander Hamilton", "John Adams" ], Answer: 2),
                      Question(Question: "How many World Series did Yogi Berra win as a player?", Answers: ["11", "10", "5", "7" ], Answer: 1),
-                     Question(Question: "Which three countries hosted the Winter Olympics during the 1990's?", Answers: ["Norway, France, Russia", "US, Sweeden, Canada", "Japan, Canada, Germany", "Slovenia, France, South Korea" ], Answer: 0),
+                     Question(Question: "Which three countries hosted the Winter Olympics during the 1990's?", Answers: ["Norway, France, Russia", "US, Sweden, Canada", "Japan, Canada, Germany", "Slovenia, France, South Korea" ], Answer: 0),
                      Question(Question: "Which of these foods is high in Vitamin C?", Answers: ["Carrot", "Potato", "Corn", "Tomato" ], Answer: 3),
                      Question(Question: "How did John D. Rockefeller make his fortune?", Answers: ["Steel Industry", "Car Industry", "Oil Industry", "RailRoads" ], Answer: 2),
                      Question(Question: "Who won the NBA Finals in 2014-2015?", Answers: ["Golden State Warriors", "Clevland Cavaliers", "Chicago Bulls", "Miami Heat" ], Answer: 3),
                      Question(Question: "How many branches of government are there in the US?", Answers: ["2", "1", "3", "4" ], Answer: 2),
                      Question(Question: "What were two common foods eaten by Americans during the Great Depression?", Answers: ["Soup and Bread", "Beans and Bread", "Potatos and Soup", "Bread and Potatos" ], Answer: 1),
                      //                     Question(Question: "Where are the Aleutian Islands?", Answers: ["Alaska, USA", "Northwest Territory, Canada", "Siberia, Russia", "Hawaii"], Answer: 1),
-            Question(Question: "Which of the following NBA players below was drafted first overall?", Answers: ["Micheal Jordan", "Stephen Curry", "Kevin Durant", "Lebron James"], Answer: 3),]
+            Question(Question: "Which of the following NBA players below was drafted first overall?", Answers: ["Michael Jordan", "Stephen Curry", "Kevin Durant", "Lebron James"], Answer: 3),]
         
     }
     func pickQuestions(){
@@ -118,10 +145,14 @@ class TestViewController: UIViewController {
             AnswerNumber = Questions[QNumber].Answer
             
             for i in 0..<Buttons.count{
-                button1.setTitle(Questions[QNumber].Answers[0], for: UIControlState())
-                button2.setTitle(Questions[QNumber].Answers[1], for: UIControlState())
-                button3.setTitle(Questions[QNumber].Answers[2], for: UIControlState())
-                button4.setTitle(Questions[QNumber].Answers[3], for: UIControlState())
+                //button1.setTitle(Questions[QNumber].Answers[0], for: UIControlState())
+                labelButton1.text = Questions[QNumber].Answers[0]
+                //button2.setTitle(Questions[QNumber].Answers[1], for: UIControlState())
+                labelButton2.text = Questions[QNumber].Answers[1]
+                //button3.setTitle(Questions[QNumber].Answers[2], for: UIControlState())
+                labelButton3.text = Questions[QNumber].Answers[2]
+                //button4.setTitle(Questions[QNumber].Answers[3], for: UIControlState())
+                labelButton4.text = Questions[QNumber].Answers[3]
                 
             }
             Questions.remove(at: QNumber)
@@ -130,7 +161,7 @@ class TestViewController: UIViewController {
             
         else{
             timer.invalidate()
-            theEnd.text = "You Win!"
+            theEnd.text = "Click Restart to Play Again"
             theEnd.alpha = 1
             button1.isEnabled = false
             button2.isEnabled = false
@@ -199,6 +230,7 @@ class TestViewController: UIViewController {
         button3.isEnabled = true
         button2.isEnabled = true
         button1.isEnabled = true
+
         
     }
     func updateTimer(){
@@ -213,10 +245,10 @@ class TestViewController: UIViewController {
         if AnswerNumber == 0{
             
             pickQuestions()
-            
+            findCurrentQuestion()
         }
         else{
-            incorrectLabel.text = "You are Incorrect!"
+            incorrectLabel.text = "Incorrect!"
             incorrectLabel.alpha = 1
             score += 1
             scoreLbl.text = "\(score)"
@@ -230,10 +262,10 @@ class TestViewController: UIViewController {
         if AnswerNumber == 1{
             
             pickQuestions()
-            
+            findCurrentQuestion()
         }
         else{
-            incorrectLabel.text = "You are Incorrect!"
+            incorrectLabel.text = "Incorrect!"
             incorrectLabel.alpha = 1
             score += 1
             scoreLbl.text = "\(score)"
@@ -244,10 +276,10 @@ class TestViewController: UIViewController {
         if AnswerNumber == 2{
             
             pickQuestions()
-            
+            findCurrentQuestion()
         }
         else{
-            incorrectLabel.text = "You are Incorrect!"
+            incorrectLabel.text = "Incorrect!"
             incorrectLabel.alpha = 1
             score += 1
             scoreLbl.text = "\(score)"
@@ -259,10 +291,11 @@ class TestViewController: UIViewController {
         if AnswerNumber == 3{
             
             pickQuestions()
-            
+            findCurrentQuestion()
         }
+            
         else{
-            incorrectLabel.text = "You are Incorrect!"
+            incorrectLabel.text = "Incorrect!"
             incorrectLabel.alpha = 1
             score += 1
             scoreLbl.text = "\(score)"
